908eb1a9aa24619ed55b11190f326b3e7f77ef11
jeet23 <jeet.23nz@gmail.com>
2014-06-12 12:10:44 +0530

    Total changes today by Jeet upto 12 noon

--- webportal/settings.py
+++ webportal/settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'webapp_db',                      # Or path to database file if using sqlite3.
+        'NAME': 'django_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'admin',
-        'PASSWORD': 'admin',
+        'USER': 'jeet',
+        'PASSWORD': 'jeet',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
