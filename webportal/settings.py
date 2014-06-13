5e81de0521d7d6219cf0e717f9b51a7835dde7ee
priyanjasingh <priyanjasingh15@gmail.com>
2014-06-13 15:18:58 +0530

    sqlite configured

--- webportal/settings.py
+++ webportal/settings.py
@@ -25,8 +25,8 @@ DATABASES = {
         'NAME': 'webportal.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
 
-        'USER': 'admin',
-        'PASSWORD': '1',
+        'USER': '',
+        'PASSWORD': '',
         
 
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
