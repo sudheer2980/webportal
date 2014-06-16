66c9fc372cc016f0b680115daa4e7102706302d8
Sudheer <sudhirmacherla1993@gmail.com>
2014-06-16 18:34:12 +0530

    Populate files

---
+++ subject_list.py
@@ -0,0 +1,57 @@
+s2 = {  'class' = '7',
+	'subject' = 'maths',
+	'topic' = 'Profit and loss',
+	'pdf' = 'profitloss.pdf',
+	'video' = '',
+	'animation' = '',
+	'summary' ='aptitude related'
+     }
+s3 = {  'class' = '8',
+	'subject' = 'maths',
+	'topic' = 'statistics',
+	'pdf' = 'statistics.pdf',
+	'video' = '',
+	'animation' = '',
+	'summary' ='aptitude related'
+     }
+s4 = {  'class' = '8',
+	'subject' = 'science',
+	'topic' = 'Human body',
+	'pdf' = 'humanbody.pdf',
+	'video' = '',
+	'animation' = '',
+	'summary' ='Related to Human'
+     }
+s5 = {  'class' = '9',
+	'subject' = 'maths',
+	'topic' = 'Probability',
+	'pdf' = 'probability.pdf',
+	'video' = '',
+	'animation' = '',
+	'summary' ='aptitude related'
+     }
+s6 = {  'class' = '9',
+	'subject' = 'science',
+	'topic' = 'Chemical Bond',
+	'pdf' = 'chem_bond.pdf',
+	'video' = '',
+	'animation' = '',
+	'summary' ='Relatedto chemistry'
+     }
+s7 = {  'class' = '10',
+	'subject' = 'maths',
+	'topic' = 'Permutations',
+	'pdf' = 'permutations.pdf',
+	'video' = '',
+	'animation' = '',
+	'summary' ='Permutations and combinations'
+     }
+s8 = {  'class' = '10',
+	'subject' = 'science',
+	'topic' = 'Heat and temparature',
+	'pdf' = 'heat_temp.pdf',
+	'video' = '',
+	'animation' = '',
+	'summary' ='Physical Science'
+     }
+subjects=[s1,s2,s3,s4,s5,s6,s7,s8]
